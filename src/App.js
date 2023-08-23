668f47b42bc9e1c984d82f8768a6666fb52b0076
jkokilaCSE <jkokila2810@gmail.com>
2023-08-23 21:03:46 +0530

    props in simple way

--- src/App.js
+++ src/App.js
@@ -2,23 +2,32 @@ import Navbar from "./Navbar";
 import Hero from "./Hero"
 import Card from "./Card"
 import data from "./data.js"
+
 export default function App(){
 
   const card = data.map(item=>{
     return (
       <Card 
-    img={item.coverImg}
-    rating={item.stats.rating}
-    reviewCount={item.stats.reviewCount}
-    country={item.location}
-    Title={item.title}
-    price={item.price} />
+    key={item.id}
+    // img={item.coverImg}
+    // rating={item.stats.rating}
+    // reviewCount={item.stats.reviewCount}
+    // country={item.location}
+    // Title={item.title}
+    // price={item.price}
+    // openSpots={item.openSpots} we can do it simple like
+    // {...item}this line can be replace for the below line that is not telling the particular props and also not mensioning the props name in that particular file.
+    item={item}
+    />
     )
   })
   return(
     <>
     <Navbar />
+    <Hero />
+    <section className="cards-list">
     {card}
+    </section>
     </>
   )
 }
--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,23 @@
 export default function Card(props){
+    let badgeText
+    if(props.item.openSpots===0){
+        badgeText="SOLD OUT"
+    }
+    else if(props.item.location==="Online"){
+        badgeText="ONLINE"
+    }
     return(
         <div className="card">
-            <img src={`../${props.img}`} className="card--image"/>
+            {badgeText&&<div className="card--badge ">{badgeText}</div>}
+            <img src={`../${props.item.coverImg}`} className="card--image"/>
             <div className="card--stats">
                 <img src="../star.jpg" className="card--star" />
-                <span className="">{props.rating}</span>
-                <span className="gray">({props.reviewCount}) .</span>
-                <span className="gray">{props.country}</span>
+                <span className="">{props.item.stats.rating}</span>
+                <span className="gray">({props.item.stats.reviewCount}) .</span>
+                <span className="gray">{props.item.location}</span>
             </div>
-            <p>{props.Title}</p>
-            <p><span className="bold">From ${props.price}</span>/ person</p>
+            <p className="card--title">{props.item.title}</p>
+            <p className="card--price"><span className="bold">From ${props.item.price}</span>/ person</p>
         </div>
     )
 }
